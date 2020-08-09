7a2155a3e40351bac0e5488ae6801c6764e1620c
arsil-z <arsilzunzunia@gmail.com>
2020-08-09 17:50:55 +0530

    Implemented web scrapping of detail page and bringing data to front-end

--- web_scraper/views.py
+++ web_scraper/views.py
@@ -1,26 +1,42 @@
 from django.shortcuts import render, HttpResponse
-import requests
+from .utils import _get_url_to_fetch_data
 
 # Create your views here.
-from .article_list_data import execute
+from .article_list_data import get_url_on_search, _get_article_detail_url
+from .article_detail_data import _get_detail_page_url_by_id
+
 
 def article_list_view(request):
 	response = ""
+	search_query = ""
+	tags = ""
 	if request.method == "POST":
 		search_query = request.POST["search_query"]
-		source_url = get_url_to_fetch_data(search_query)
-		response = execute(source_url)
+		source_url = _get_url_to_fetch_data(search_query)
+		response, tags = get_url_on_search(source_url)
 	context = {
-		'response': response
+		'response': response,
+		'search_query': search_query,
+		"tags": tags
 	}
 	return render(request, "web_scraper/home.html", context)
 
-def get_url_to_fetch_data(string):
-	source = requests.get(f'https://medium.com/search?q={string}').text
-	return source
 
-# def article_detail_view(request, id):
-# 	get_artilce_detail_page_url_by_id = get_artilce_detail_page_url(id)
-# 	p_this = get_blog_detail(get_artilce_detail_page_url_by_id)
-# 	print("test", p_this)
-# 	return HttpResponse("Done")
+def article_detail_view(request, search_query, key):
+	query_url = _get_url_to_fetch_data(search_query)
+	blog_detail_dictionary = _get_detail_page_url_by_id(query_url, key)
+	context = {
+		"blog_detail_dictionary": blog_detail_dictionary
+	}
+	return render(request, "web_scraper/blog_detail.html", context)
+
+
+def article_search_by_tag(request, search_query):
+	source_url = _get_url_to_fetch_data(search_query)
+	response, tags = get_url_on_search(source_url)
+	context = {
+		'response': response,
+		'search_query': search_query,
+		"tags": tags
+	}
+	return render(request, "web_scraper/result_by_tag.html", context)
